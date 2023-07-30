0ef3f9e07ec6f0942abbdafb7cc9abaa243075f9
Laurence Hook <hookla1976@gmail.com>
2023-07-30 21:34:32 +0100

    initial

---
+++ connect4.py
@@ -0,0 +1,48 @@
+class Connect4:
+    def __init__(self):
+        self.board = self.create_board()
+        self.game_over = False
+        self.winner = None
+
+    def create_board(self):
+        # The board has 6 rows and 7 columns
+        return [[0 for _ in range(7)] for _ in range(6)]
+
+    def is_valid_move(self, col):
+        # A move is valid if the top row of a column is not occupied
+        return self.board[0][col] == 0
+
+    def get_valid_moves(self):
+        return [c for c in range(7) if self.is_valid_move(c)]
+
+    def make_move(self, col, piece):
+        if self.is_valid_move(col):
+            for r in range(5, -1, -1):
+                if self.board[r][col] == 0:
+                    self.board[r][col] = piece
+                    break
+            if self.is_winner(piece):
+                self.game_over = True
+                self.winner = piece
+            return True
+        else:
+            return False
+
+    def is_winner(self, piece):
+        # Check horizontal, vertical and diagonal directions
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for r in range(6):
+            for c in range(7):
+                if self.board[r][c] == piece:
+                    for dr, dc in directions:
+                        if self.check_direction(r, c, dr, dc, piece):
+                            return True
+        return False
+
+    def check_direction(self, r, c, dr, dc, piece):
+        # Check four positions in the given direction
+        for i in range(1, 4):
+            nr, nc = r + dr * i, c + dc * i
+            if not (0 <= nr < 6 and 0 <= nc < 7) or self.board[nr][nc] != piece:
+                return False
+        return True
---
+++ tests/test_connect4.py
@@ -0,0 +1,45 @@
+import unittest
+
+from connect4 import Connect4
+
+
+class TestConnect4(unittest.TestCase):
+    def test_create_board(self):
+        game = Connect4()
+        self.assertEqual(len(game.board), 6)
+        self.assertEqual(len(game.board[0]), 7)
+        self.assertEqual(sum(sum(row) for row in game.board), 0)
+
+    def test_valid_move(self):
+        game = Connect4()
+        self.assertTrue(game.is_valid_move(0))
+        game.make_move(0, 1)
+        self.assertTrue(game.is_valid_move(0))
+        for _ in range(5):
+            game.make_move(0, 1)
+        self.assertFalse(game.is_valid_move(0))
+
+    def test_game_over(self):
+        game = Connect4()
+        self.assertFalse(game.game_over)
+        for _ in range(4):
+            game.make_move(0, 1)
+        self.assertTrue(game.game_over)
+        self.assertEqual(game.winner, 1)
+
+    def test_check_direction(self):
+        game = Connect4()
+        game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [1, 2, 0, 0, 0, 0, 0],
+            [1, 2, 2, 0, 2, 1, 0],
+            [1, 1, 2, 0, 1, 1, 0],
+            [2, 2, 1, 1, 2, 1, 0],
+        ]
+        self.assertTrue(game.is_winner(1))  # Vertical win for player 1
+        self.assertFalse(game.is_winner(2))  # No win for player 2
+
+
+if __name__ == "__main__":
+    unittest.main()
